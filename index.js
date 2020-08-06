e9a142bd5b0ef82545b34b7e1ed8715118cdb499
seed-ganesh <ecofriendganesh@gmail.com>
2020-08-07 01:51:39 +0530

    method changed

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     // }
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", 'GET', 'POST');
+    res.header("Access-Control-Allow-Methods", '*' );
     next();
 });
 
